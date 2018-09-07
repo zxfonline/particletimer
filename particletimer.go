ab7d33f7346cb9eb5a4ba27dff4dcaf06a941566
zxfonline <zxfonline@sina.com>
2018-09-07 17:43:34 +0800

    优化代码

--- particletimer.go
+++ particletimer.go
@@ -12,7 +12,6 @@ import (
 
 	"github.com/zxfonline/chanutil"
 	"github.com/zxfonline/golog"
-	"github.com/zxfonline/timefix"
 )
 
 type _timer_event struct {
@@ -27,14 +26,14 @@ const (
 )
 
 var (
-	logger     *golog.Logger                       = golog.New("ParticleTimer")
-	_eventlist [TIMER_LEVEL]map[int64]_timer_event // 事件列表
+	logger     *golog.Logger                        = golog.New("ParticleTimer")
+	_eventlist [TIMER_LEVEL]map[int64]*_timer_event // 分级事件列表
 
-	_eventqueue      map[int64]_timer_event // 事件添加队列
+	_eventqueue      map[int64]*_timer_event // 新增事件添加队列
+	_alleventqueue   map[int64]*_timer_event // 所有事件添加队列
 	_eventqueue_lock sync.Mutex
 
-	_deleted_timers      map[int64]bool
-	_deleted_timers_lock sync.Mutex
+	_deleted_timers map[int64]bool
 
 	_timer_id int64 // 内部事件编号
 
@@ -44,9 +43,10 @@ var (
 
 func init() {
 	for k := range _eventlist {
-		_eventlist[k] = make(map[int64]_timer_event)
+		_eventlist[k] = make(map[int64]*_timer_event)
 	}
-	_eventqueue = make(map[int64]_timer_event)
+	_eventqueue = make(map[int64]*_timer_event)
+	_alleventqueue = make(map[int64]*_timer_event)
 	_deleted_timers = make(map[int64]bool)
 	stopD = chanutil.NewDoneChan()
 	go _timer()
@@ -64,7 +64,7 @@ func Close() {
 
 //当前修正 UTC时间 毫秒
 func Current() int64 {
-	return timefix.MillisUTCTime()
+	return time.Now().UnixNano() / 1e6
 }
 
 // 定时器 根据程序启动后经过的秒数计数
@@ -80,10 +80,10 @@ func _timer() {
 			q = true
 		default:
 			time.Sleep(TICK_TIME * time.Millisecond)
+			_eventqueue_lock.Lock()
 			if len(_eventqueue) > 0 {
 				// 处理排队
 				// 最小的时间间隔，处理为1ms
-				_eventqueue_lock.Lock()
 				for k, v := range _eventqueue {
 					// 处理微小间隔
 					diff := v.Timeout - Current()
@@ -98,9 +98,9 @@ func _timer() {
 						}
 					}
 				}
-				_eventqueue = make(map[int64]_timer_event)
-				_eventqueue_lock.Unlock()
+				_eventqueue = make(map[int64]*_timer_event)
 			}
+			_eventqueue_lock.Unlock()
 			// 检查事件触发
 			// 累计距离上一次触发的秒数,并逐秒触发
 			// 如果校正了系统时间，时间前移，nmsecs为负数的时候，last的值不应该变动，否则会出现秒数的重复计数
@@ -133,16 +133,23 @@ func _trigger(level uint) {
 	for k, v := range list {
 		if v.Timeout-now < 1<<level {
 			if level == 0 {
-				func() {
+				func(tk int64, tv *_timer_event) {
 					defer func() {
 						if err := recover(); err != nil {
-							logger.Warnf("TRIGGER err:%+v channel len:%d", err, len(v.ch))
+							logger.Errorf("TRIGGER err:%+v channel len:%d\n", err, len(tv.ch))
 						}
 					}()
-					if checkValidTimer(v.Id) {
-						v.ch <- v.Id
+
+					_eventqueue_lock.Lock()
+					defer _eventqueue_lock.Unlock()
+					delete(_alleventqueue, tv.Id)
+					if checkValidTimer(tv.Id) {
+						tv.ch <- tv.Id
+						// logger.Debugf("trigger timer ok, timerid:%d,ptid:%d,timeout:%d", tv.Id, tk, tv.Timeout)
+					} else {
+						// logger.Warnf("trigger timer fail, timerid:%d,ptid:%d,timeout:%d", tv.Id, tk, tv.Timeout)
 					}
-				}()
+				}(k, v)
 			} else { // 移动到前一个更短间距的LIST
 				_eventlist[level-1][k] = v
 			}
@@ -152,31 +159,54 @@ func _trigger(level uint) {
 }
 
 // 添加一个定时事件，timeout为到期的修正UTC时间 毫秒  id 是调用者定义的编号(请使用唯一id生成器生成), 事件发生时，会把id发送到ch
-func Add(id int64, timeout int64, ch chan int64) {
+func Add(id int64, timeout int64, ch chan int64) int64 {
 	if atomic.LoadInt32(&state) != 1 {
 		panic(errors.New("particle timer closed"))
 	}
-	timer_id := atomic.AddInt64(&_timer_id, 1)
-	event := _timer_event{Id: id, ch: ch, Timeout: timeout}
 	_eventqueue_lock.Lock()
-	_eventqueue[timer_id] = event
-	_eventqueue_lock.Unlock()
+	defer _eventqueue_lock.Unlock()
+	_timer_id++
+	event := &_timer_event{Id: id, ch: ch, Timeout: timeout}
+	_eventqueue[_timer_id] = event
+	_alleventqueue[id] = event
+	// logger.Debugf("add timer ok, timerid:%d,ptid:%d,timeout:%d", id, _timer_id, timeout)
+	return _timer_id
+}
+
+func ParticleTimerExist(id int64) bool {
+	_eventqueue_lock.Lock()
+	defer _eventqueue_lock.Unlock()
+	_, exists := _alleventqueue[id]
+	return exists
+}
+
+func ParticleTimerInfo(ptid int64) (bool, int) {
+	_eventqueue_lock.Lock()
+	defer _eventqueue_lock.Unlock()
+	for level, mp := range _eventlist {
+		for k := range mp {
+			if k == ptid {
+				return true, level
+			}
+		}
+	}
+	return false, 0
 }
 
 //删除指定id的定时事件
 func Del(id int64) {
-	_deleted_timers_lock.Lock()
-	_deleted_timers[id] = true
-	_deleted_timers_lock.Unlock()
+	_eventqueue_lock.Lock()
+	defer _eventqueue_lock.Unlock()
+	if _, exists := _alleventqueue[id]; exists {
+		_deleted_timers[id] = true
+	}
 }
 
 func checkValidTimer(id int64) bool {
 	flag := true
-	_deleted_timers_lock.Lock()
-	if deleted, _ := _deleted_timers[id]; deleted {
+	if _, deleted := _deleted_timers[id]; deleted {
 		flag = false
+		delete(_deleted_timers, id)
 	}
-	delete(_deleted_timers, id)
-	_deleted_timers_lock.Unlock()
 	return flag
 }
